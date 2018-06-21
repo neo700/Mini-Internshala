f971c67f24ac4edaa1b186549238c127d56ecefd
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2018-06-21 07:50:54 +0530

    Update db_conection.php

--- db_conection.php
+++ db_conection.php
@@ -1,8 +1,8 @@
 <?php
 
-$server="localhost";
-$user="root";
-$pass="";
+$server="us-cdbr-iron-east-04.cleardb.net";
+$user="bb1b721b07451c";
+$pass="d3441686";
 $con=mysqli_connect($server,$user,$pass);
 if(!$con){
 	
@@ -10,7 +10,7 @@ if(!$con){
 		
 }
 
-$db=mysqli_select_db($con,"assignment");
+$db=mysqli_select_db($con,"heroku_0b7857f57f33cec");
 if(!$db){
 	die("Connection failed: " . $db->connect_error);
 			
