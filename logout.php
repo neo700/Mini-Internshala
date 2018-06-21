f48b4843dbacf89173c6a513987b2c6382864a42
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2018-06-21 11:56:02 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 
 session_start();
 session_destroy();  
-header('location: login.html');//use for the redirection to some page  
+header('location: index.html');//use for the redirection to some page  
 
 ?>  
