735ae8648a30417a5cbc8c27ac35ce63adb4954c
Sarthak Aggarwal <sarthaakagarwal@gmail.com>
2018-06-21 19:33:01 +0530

    Add files via upload

--- show_stu.php
+++ show_stu.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
 include("db_conection.php");
+
 if($_SESSION['mode']=="EMPLOYER")
 {
     echo "<script>";
-		 echo "alert('PAGE NOT ALLOWED    LOGIN AGAIN');";
-		 
+         echo "alert('PAGE NOT ALLOWED    LOGIN AGAIN');";
+         
 
 
-		  echo "window.location.href = 'login.html'";
+          echo "window.location.href = 'login.html'";
     echo "</script>";
 
-}	
+}   
 
 
 ?>
@@ -39,15 +40,16 @@ if($_SESSION['mode']=="EMPLOYER")
 <div class="row">
   <header class="main-header">
     
-	  <h1 style="color: white">Internshala</h1>
-	  <div style="float: right; margin-right: 80px;" >
+	  <h1 style="color: white;">Internshala</h1>
+	  
+<div style="float: right; margin-right: 80px;" >
 
 <form action="logout.php">
-  	  <button type="submit" class="btn btn-primary btn-danger" style="text-align:center;" name="reg_user" >Logout</button>
-  	
+      <button type="submit" class="btn btn-primary btn-danger" style="text-align:center;" name="reg_user" >Logout</button>
+    
 </form>
 </div>
-	  <h1 style="float:right; margin-right:20px">HEY, 
+      <h1 style="float:right; margin-right:20px">HEY, 
   <?php
 include("db_conection.php");
 echo $_SESSION['username'];
@@ -56,56 +58,62 @@ echo $_SESSION['username'];
 </header>
 </div>
 
-<form action="dashboard_stu_reg.php">
+
+
+
+
+       <form action="dashboard_stu_reg.php"  >
 
       <button type="submit" class="btn" name="reg_user" >Register Now</button>
-    
-</form>
-<form action="show_stu.php">
 
-      <button type="submit" class="btn" name="reg_user" >View Internships</button>
-    
 </form>
-<br><br>
-<br><br>
 
-<div class="content" >
-    <div class="ranking" style="margin-top:10px;">
-	    <center>
-        <h2 class="ranking-title">Internship's Applied</h2>
+<div class="content">
+
+
+    <div class="ranking " style="margin-top:100px;">
+<div class="row" >
+ 
+</div>
+</div>
+</div>
+<div class="content">
+  <div class="ranking">
+
+        <h2 class="ranking-title">Intership Posted</h2>
         <table class="table-ranking">
             <tr>
-            	<th class="table-ranking-head">Internship Id</th>
+                <th class="table-ranking-head">Internship Id</th>
                 <th class="table-ranking-head">Company Name</th>
-                <th class="table-ranking-head">Apply </th>
-                <th class="table-ranking-head">Information Submitted</th>
+               
+                <th class="table-ranking-head">Descriptiom</th>
+                <th class="table-ranking-head">Apply By</th>
+
             </tr>
-	
-	    </center>
 <?php
 
 
-include("db_conection.php");
-
-$id=$_SESSION['username'];
 
 
-$q="SELECT id,com_name, dob, id_stu, stu_info FROM internship_reg WHERE id_stu='$id'";
+
+$q="SELECT * from internship_post";
 $w=mysqli_query($con,$q);
 if ($w->num_rows > 0) {
     // output data of each row
     while($row = $w->fetch_assoc()) {
         echo '<tr class="table-ranking-content">
         		<td>'. $row["id"].'</td>
-        		<td>'. $row["com_name"].'</td>
-        		<td>'. $row["dob"].'</td>
-        		<td>'. $row["stu_info"].'</td>
+            <td>'. $row["com_name"].'</td>
+        	
+        		<td>'. $row["description"].'</td>
+        		<td>'. $row["apply_by"].'</td> 
         	</tr>';
     }
-}
-
+} 
 ?>
+</div>
+</div>
 
-	    
+
 </body></html>
 
